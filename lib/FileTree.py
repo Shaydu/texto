66afc55822c7bb1129539bca37dd4d6f7a4f9666
Stephen Haydu <shaydu@gmail.com>
2016-07-12 22:33:40 -0700

    added dir looping and video dimensions

---
+++ lib/FileTree.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+import glob
+from VideoAnalyzer import VideoAnalyzer
+
+class FileTree:
+
+     @staticmethod
+     def outputTree(filePath):
+         if(os.path.isdir(filePath)):
+             out = subprocess.check_output(["tree","-lhat", filePath])
+
+             vidExt = ['.mkv','.m4v','.mp4']
+             
+             dirs =  os.listdir(filePath)
+             for filename in dirs:
+                filename2, fileExt = os.path.splitext(filename)
+                print("loop filext" + fileExt)
+                print("loop filename" + filename)
+                if(fileExt in vidExt):
+                     out += "vid size: " + VideoAnalyzer.getVideoSize(filePath + "/" + filename) + "\n"
+  
+         else:
+             out = subprocess.check_output(["ls","-lhat", filePath]) 
+         print("returning " + out)
+         return out
+
+ft = FileTree()
+
+#print ft.outputTree("/mnt/media/complete/Antichrist.Criterion.Collection.UNCUT.Edition.2009.BluRay.720p.DTS.x264-CHD/")              
+#print ft.outputTree("/mnt/media/complete/Wedding.m4v")              
